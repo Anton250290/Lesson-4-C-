61991799e29bc9e7b1763ce10a4b07fcc0224b09
Anton250290 <146120168+Anton250290@users.noreply.github.com>
2023-11-12 21:54:26 +0300

    lesson4

---
+++ 27/Program.cs
@@ -0,0 +1,40 @@
+﻿// // Задача 27: Напишите программу, которая принимает на вход число 
+// // и выдаёт сумму цифр в числе.
+
+
+
+int Prompt (string str) 
+ {
+     Console.WriteLine(str);
+     int number = Convert.ToInt32(Console.ReadLine());
+     return number;
+ }
+
+ int Length (int number)
+ {
+     int ind = 0;
+     while (number > 0)
+     {
+         number = number / 10;
+         ind++;
+     }
+     return ind;
+ }
+
+ void sum (int b, int len)
+
+ {
+     int sum = 0;
+     for (int i = 1; i <= len; i++)
+     {
+         sum += b % 10;
+         b = b / 10; 
+     }
+     Console.WriteLine($"Сумма цифр в числе A равна: " + sum);
+ }
+
+int a = Prompt("Введите число А: ");
+int len = Length(a);
+sum(a, len);
+ 
+
